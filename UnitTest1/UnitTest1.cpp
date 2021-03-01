319f0c4ed4c4173f80b929031237c51d78f47652
Аня Ясинецька <anna.yasinetska@gmail.com>
2021-03-01 23:36:51 +0200

    first

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../1.3/Money.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Money money;
+			bool t;
+			t = money.Init(1000, 500, 200, 100, 50, 20, 10, 5, 2, 1, 50, 10);
+			Assert::AreEqual(t, true);
+
+		}
+	};
+}
